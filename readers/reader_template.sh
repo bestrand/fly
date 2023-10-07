df8b75b1d6fbceaf1e737b3b6baaea75d9dd7929
Bjørn-Erik Strand <bjoern.erik.strand@gmail.com>
2023-10-07 22:00:37 +0200

    Add mqtt config

--- readers/reader_template.sh
+++ readers/reader_template.sh
@@ -10,12 +10,12 @@ TOOL_CMD="$TOOL_CMD"
 echo "Reading \$HEADLINE" 
 
 if [ ! -d "\$REPO" ] ; then
-    git clone "\${OWNER}/\${REPO}.git" "/root/\$REPO"
-    cd /root/\$REPO
+    git clone "\${OWNER}/\${REPO}.git" "\$REPO"
+    cd \$REPO
     \$TOOL_CMD \$HEADLINE
 fi
 
-cd /root/\$REPO
+cd \$REPO
 git fetch --quiet
 
 if git diff HEAD origin/main --quiet -- \$HEADLINE; then
