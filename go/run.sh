841187f15440c50a59ca0c2e64a3546e125465ce
Bjørn-Erik Strand <bjoern.erik.strand@gmail.com>
2023-11-04 14:03:55 +0100

    Update run.sh

--- go/run.sh
+++ go/run.sh
@@ -1,4 +1,5 @@
 apk add --no-cache go
+mkdir -p fly/go && cd fly/go/
 wget https://raw.githubusercontent.com/bestrand/fly/c65217eea3461aa3c06249fdf0a509bee37dfb84/go/main.go
 go mod init main && go mod tidy
 go run .
