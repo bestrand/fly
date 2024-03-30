1d6b7bb9862f72e8e95d10cf5e690c56faebfebc
Bjørn-Erik Strand <bjoern.erik.strand@gmail.com>
2024-03-30 17:37:19 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 apk add curl git
-git clone https://github.com/bestrand/fly.git
+git clone https://github.com/bestrand/fly.git && cd fly
 curl -L https://fly.io/install.sh | sh
 chmod +x fly-deploy.sh
-./fly-deploy.sh
+./fly-deploy.sh alpine-clean
