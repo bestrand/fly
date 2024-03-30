34d8dc2c89faeea29b332d325095860abd30b1bb
Bjørn-Erik Strand <bjoern.erik.strand@gmail.com>
2024-03-30 17:23:55 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 apk add curl git
-git clone git@github.com:bestrand/fly.git
+git clone https://github.com/bestrand/fly.git
 curl -L https://fly.io/install.sh | sh
 chmod +x fly-deploy.sh
 ./fly-deploy.sh
