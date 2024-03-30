901b3b46a963f0e96250be0f3c320ab767ac3a6d
Bjørn-Erik Strand <bjoern.erik.strand@gmail.com>
2024-03-30 17:19:03 +0100

    Update fly-deploy.sh

--- fly-deploy.sh
+++ fly-deploy.sh
@@ -1,10 +1,10 @@
 APP_NAME="$1"
-APP_EXISTS=$(flyctl apps list -t $FLY_TOKEN | grep $APP_NAME)
+APP_EXISTS=$(/root/.fly/bin/flyctl apps list -t $FLY_TOKEN | grep $APP_NAME)
  
 if [ -z "$APP_EXISTS" ]; then
   echo "App $1 does not exist, creating..."
-  flyctl launch --detach --now --copy-config -y -t $FLY_TOKEN --ha=false
+  /root/.fly/bin/flyctl launch --now --copy-config -y -t $FLY_TOKEN --ha=false
 fi
  
 echo "Deploying app $1"
-flyctl deploy --detach --now -y -t $FLY_TOKEN --ha=false
+/root/.fly/bin/flyctl deploy --now -y -t $FLY_TOKEN --ha=false
