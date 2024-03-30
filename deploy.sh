e3df08252441a1d4efc16aeed1ebab64b5ca2a73
Bjørn-Erik Strand <bjoern.erik.strand@gmail.com>
2024-03-30 18:15:23 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ git clone https://github.com/bestrand/fly.git && cd fly
 curl -L https://fly.io/install.sh | sh
 chmod +x fly-deploy.sh
 cd locations/amsterdam/fly/mqtt
-/fly/fly-deploy.sh alpine-clean-2
+/fly/fly-deploy.sh alpine-clean
